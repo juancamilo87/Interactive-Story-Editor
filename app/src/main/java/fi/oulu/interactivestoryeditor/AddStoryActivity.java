4cccafbd4badbaecce71173154dab8588c02b218
Zhanna Sarsenbayeva <zhanna.sarsenbayeva@gmail.com>
2015-06-21 18:02:11 +0300

    Add story Activitivity

--- app/src/main/java/fi/oulu/interactivestoryeditor/AddStoryActivity.java
+++ app/src/main/java/fi/oulu/interactivestoryeditor/AddStoryActivity.java
@@ -22,6 +22,7 @@ public class AddStoryActivity extends Activity {
     private EditText email_edit;
     private ListView chapters_list;
     private Button btn_save;
+    private Button btn_add_chapter;
 
     private String title;
     private String summary;
@@ -35,6 +36,19 @@ public class AddStoryActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_story);
+
+        author = new Author();
+
+        title_edit = (EditText) findViewById(R.id.story_edt_title);
+        summary_edit = (EditText) findViewById(R.id.story_edt_summary);
+        name_edit = (EditText) findViewById(R.id.author_edt_name);
+        lastname_edit = (EditText) findViewById(R.id.author_edt_lastname);
+        website_edit = (EditText) findViewById(R.id.author_edt_website);
+        email_edit = (EditText) findViewById(R.id.author_edt_email);
+
+        chapters_list = (ListView) findViewById(R.id.chapters_list);
+        btn_save = (Button) findViewById(R.id.story_btn_save);
+        btn_add_chapter = (Button) findViewById(R.id.story_btn_add_chapter);
 
 
     }
