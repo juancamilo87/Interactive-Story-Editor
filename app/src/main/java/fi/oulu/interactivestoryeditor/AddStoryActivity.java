263453154b16eb83953de0c0542c60542c2aad23
Zhanna Sarsenbayeva <zhanna.sarsenbayeva@gmail.com>
2015-06-21 19:46:29 +0300

    Add story Activitivity

--- app/src/main/java/fi/oulu/interactivestoryeditor/AddStoryActivity.java
+++ app/src/main/java/fi/oulu/interactivestoryeditor/AddStoryActivity.java
@@ -3,18 +3,26 @@ package fi.oulu.interactivestoryeditor;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 import fi.oulu.interactivestoryeditor.model.Author;
+import fi.oulu.interactivestoryeditor.model.Chapter;
+import fi.oulu.interactivestoryeditor.model.Story;
 
 
 public class AddStoryActivity extends Activity {
 
 
     private Author author;
+    private Story story;
+    private ArrayList<Chapter> chapters;
 
     private EditText title_edit;
     private EditText summary_edit;
@@ -40,6 +48,7 @@ public class AddStoryActivity extends Activity {
         setContentView(R.layout.activity_add_story);
 
         author = new Author();
+        chapters = new ArrayList<Chapter>();
 
         title_edit = (EditText) findViewById(R.id.story_edt_title);
         summary_edit = (EditText) findViewById(R.id.story_edt_summary);
@@ -52,6 +61,25 @@ public class AddStoryActivity extends Activity {
         btn_save = (Button) findViewById(R.id.story_btn_save);
         btn_add_chapter = (Button) findViewById(R.id.story_btn_add_chapter);
 
+        title = title_edit.getText().toString();
+        summary = summary_edit.getText().toString();
+        author_name = name_edit.getText().toString();
+        author_lastname = lastname_edit.getText().toString();
+        author_website = website_edit.getText().toString();
+        author_email = email_edit.getText().toString();
+
+        author.setName(author_name);
+        author.setLast_name(author_lastname);
+        author.setWebsite(author_website);
+        author.setEmail(author_email);
+
+        ArrayAdapter<Chapter> arrayAdapter = new ArrayAdapter<Chapter>(
+                this,
+                android.R.layout.simple_list_item_1,
+                chapters);
+
+        chapters_list.setAdapter(arrayAdapter);
+
         btn_add_chapter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -61,9 +89,89 @@ public class AddStoryActivity extends Activity {
             }
         });
 
-        
+        btn_save.setOnClickListener(new View.OnClickListener(){
 
+            @Override
+            public void onClick(View view) {
+                if (verifyFields()) {
+                    story = new Story(author, title, summary);
 
+                    Intent returnIntent = new Intent();
+                    returnIntent.putExtra("story", (Parcelable) story);
+                    setResult(RESULT_OK, returnIntent);
+                    finish();
+                } else {
+                    setResult(RESULT_CANCELED);
+                    finish();
+                }
+            }
+        });
     }
+
+    private boolean verifyFields()
+    {
+        if(!title.trim().equals(""))
+        {
+            title = title_edit.getText().toString().trim();
+        }
+        else
+        {
+            return false;
+        }
+
+        if(!summary.trim().equals(""))
+        {
+            summary = summary_edit.getText().toString().trim();
+        }
+        else
+        {
+            return false;
+        }
+
+        if(!author_name.trim().equals(""))
+        {
+            author_name = name_edit.getText().toString().trim();
+        }
+        else
+        {
+            return false;
+        }
+
+        if(!author_lastname.trim().equals(""))
+        {
+            author_lastname = lastname_edit.getText().toString().trim();
+        }
+        else
+        {
+            return false;
+        }
+
+        if(!author_website.trim().equals(""))
+        {
+            author_website = website_edit.getText().toString().trim();
+        }
+        else
+        {
+            return false;
+        }
+
+        if(!author_email.trim().equals(""))
+        {
+            author_email = email_edit.getText().toString().trim();
+        }
+        else
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        setResult(RESULT_CANCELED);
+        finish();
+    }
+
 
 }
