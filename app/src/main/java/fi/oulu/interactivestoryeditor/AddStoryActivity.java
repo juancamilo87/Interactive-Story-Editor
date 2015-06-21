8242cedbaa371783cc856a3b94dfdb1597a47883
Zhanna Sarsenbayeva <zhanna.sarsenbayeva@gmail.com>
2015-06-21 16:43:28 +0300

    Add story

--- app/src/main/java/fi/oulu/interactivestoryeditor/AddStoryActivity.java
+++ app/src/main/java/fi/oulu/interactivestoryeditor/AddStoryActivity.java
@@ -2,14 +2,41 @@ package fi.oulu.interactivestoryeditor;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import fi.oulu.interactivestoryeditor.model.Author;
 
 
 public class AddStoryActivity extends Activity {
+
+
+    private Author author;
+
+    private EditText title_edit;
+    private EditText summary_edit;
+    private EditText name_edit;
+    private EditText lastname_edit;
+    private EditText website_edit;
+    private EditText email_edit;
+    private ListView chapters_list;
+    private Button btn_save;
+
+    private String title;
+    private String summary;
+    private String author_name;
+    private String author_lastname;
+    private String author_website;
+    private String author_email;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_story);
+
+
     }
 
 }
